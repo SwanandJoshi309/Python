d363b007078943ed5bffe10eaed4260e06b8680b
Swanand Joshi <96830440+SwanandJoshi309@users.noreply.github.com>
2023-07-27 09:56:57 +0530

    arithmetic operations.py
    This program shows a simple use of arithmetic operators.

---
+++ arithmetic operations.py
@@ -0,0 +1,16 @@
+#python program to perform arithmetic operations 
+
+a=10
+b=20
+
+res_sum=a+b
+print("Addition is :",res_sum)
+
+res_sub=a-b
+print("Subtraction is :",res_sub)
+
+res_mul=a*b
+print("Multiplication is :",res_mul)
+
+res_div=a/b
+print("Division is :",res_div)
