616e2eeffeba0b47a23b0fe67491080e646129a8
Swanand Joshi <96830440+SwanandJoshi309@users.noreply.github.com>
2023-07-27 10:00:18 +0530

    logical operations.py
    This program shows use of logical operators.

---
+++ logical operations.py
@@ -0,0 +1,10 @@
+#Python program to perform logical operations.
+
+print("Result of logical AND is :",(8>9) and (2<9))
+print("Result of logical AND is :",(8<9) and (2<9)) 
+
+print("\nResult of logical OR is :",(2==2) or (9<20))
+print("Result of logical OR is :",(3!=3) or (9>20))
+
+print("\nResult of logical NOT is :", not(8>2))
+print("Result of logical NOT is :", not(2>10))
