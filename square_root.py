eea0295af7c1ebf47436339246d2e778dba73203
Swanand Joshi <96830440+SwanandJoshi309@users.noreply.github.com>
2023-07-31 11:34:03 +0530

    square_root.py
    This program displays square root of the given number as an output.

---
+++ square_root.py
@@ -0,0 +1,9 @@
+#Python program to find square root of the number.
+
+import math
+def find_square_root(no):
+    return math.sqrt(no)
+
+no = int(input("Enter a number: "))
+square_root=find_square_root(no)
+print("Square root :",square_root)
