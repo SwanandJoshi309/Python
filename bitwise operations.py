a378771cd7df50c6143428af1614b0f7de765e93
Swanand Joshi <96830440+SwanandJoshi309@users.noreply.github.com>
2023-07-31 11:30:50 +0530

    bitwise operations.py
    This python program shows use of python's bitwise operators.

---
+++ bitwise operations.py
@@ -0,0 +1,8 @@
+#Python program to perform Bitwise Operations.
+
+print("Result of Bitwise And is :",(a&b))
+print("Result of Bitwise OR :",a|b)
+print("Result of Bitwise NOT :",~a)
+print("Result of Bitwise XOR :",a^b)
+print("Bitwise right shift :",a>>2)
+print("Bitwise left shift :",a<<2)
